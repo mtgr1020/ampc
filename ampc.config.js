36367391d6ebaadfbbd41be1c473d02b6789faa5
mtgr <mtgr1020@gmail.com>
2019-11-15 17:30:02 +0800

    添加命令监听

--- ampc.config.js
+++ ampc.config.js
@@ -5,8 +5,15 @@ module.exports = {
         backups: false,
     },
     sshConfig: {
+        host: '',
+        username: '',
+        password: '',
         typingmode: true,
         targetPath: '/home/zhou/software/workon/ampc',
         backups: true,
-    }
+    },
+    // commands: [{
+    //     command: 'node',
+    //     args: ['./dist/static/app.js']
+    // }]
 }
---
+++ dist/static/app.js
@@ -0,0 +1,7 @@
+var a = 0 ;
+
+
+while(a < 10){
+    console.log('child_process' + a);
+    a++;
+}
